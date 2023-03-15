a7647091ba54547bce6297240cb63819adae45e2
Myeong-hui Kim <myeonghui@myeonghuikim-Macbook.local>
2023-03-15 16:36:04 +0900

    [api-test] 데이터 10개 화면에 보여주기

--- api-test/src/App.tsx
+++ api-test/src/App.tsx
@@ -1,30 +1,38 @@
 import React, { useEffect, useState } from 'react';
 
-// interface dataType = {
-//   userId: number;
-//   id: number;
-//   title: string;
-//   compleate: boolean;
-//  };
+interface dataType {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+ };
 
 const App = () => {
-  const [todoData, setTodoData] = useState<any>([]);
+  const [todoData, setTodoData] = useState<Array<dataType>>([]);
 
   useEffect(()  => {
     fetch("https://jsonplaceholder.typicode.com/todos")
       .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setTodoData(data);
-      });
+      .then((data) => setTodoData(data));
   }, []);
+
 
   return (
     <div>
       <h1>데이터 가져오기</h1>
-      {todoData.map((item: any) => {
-        <div key={item.id}>{item}</div>
-      })}
+      <div>
+        {todoData.map((item, index) => 
+          index < 10 ? 
+          // <div key={item.id}>{JSON.stringify(item)}</div> 
+          <div key={item.id}>
+            userId: {item.userId}, 
+            id: {item.id},
+            title: {item.title},
+            completed: {String(item.completed)}
+          </div>
+          : ''
+        )}
+      </div>
     </div>
   );
 };
